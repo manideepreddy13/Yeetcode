c20e3444473521ef40504f53e0b4c3ec70ba8f93
Manideep Reddy <106183533+manideepreddy13@users.noreply.github.com>
2023-03-11 19:58:17 +0530

    Create repeat_and_missing_in_arr.cpp

---
+++ repeat_and_missing_in_arr.cpp
@@ -0,0 +1,26 @@
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    int a,b;
+    int i,j;
+    int count;
+
+    for(i = 1; i<=A.size();i++){
+        count = 0;
+        for(j=0;j<A.size();j++){
+            if(A[j] == i){
+                count++;
+            }
+        }
+
+        if(count == 2){
+            a = i;
+        } else if(count == 0) {
+            b = i;
+        }
+    }
+    
+    vector<int> vec;
+    vec.push_back(a);
+    vec.push_back(b);
+    
+    return vec;
+}
