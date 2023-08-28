e83f16586a4f099e81d83e98571f0b23d51be503
Manideep Reddy <106183533+manideepreddy13@users.noreply.github.com>
2023-08-28 14:30:19 +0530

    Time: 106 ms (57.79%), Space: 16.3 MB (11.84%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        hash_table = defaultdict(int)
+        for i in nums:
+            hash_table[i] += 1
+        
+        for i in hash_table:
+            if hash_table[i] == 1:
+                return i
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
